e3430aa2faa323ab850964486531a76732f1b7a5
ritikaj23 <ritikaj@skillup.online>
2024-05-30 06:52:26 -0400

    Hands-on Lab: Convert Static Pages to Dynamic React Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Login from './Components/Login/Login';
+import Signup from './Components/Sign_Up/Sign_Up'
+//import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation'
+//import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+//import BookingConsultation from './Components/BookingConsultation';
+//import Notification from './Components/Notification/Notification';
+//import ReviewForm from './Components/ReviewForm/ReviewForm'
+//import ProfileCard from './Components/ProfileCard/ProfileCard';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    return (
+        <div className="App">
+            <BrowserRouter>
+             
+                    <Navbar />
+                    <Routes>
+                        <Route path='/' element={<Landing_Page/>}/>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                        
+
+
+                    </Routes>
+              
+            </BrowserRouter>
+
+        </div>
+    );
+}
 export default App;
