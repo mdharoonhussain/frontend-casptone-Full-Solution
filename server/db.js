b1c145598416b065b76ab58461f58eb7844d19e6
ritikaj23 <ritikaj@skillup.online>
2024-06-28 05:40:51 -0400

    modified requirements.txt

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:Mzk3MC1yaXRpa2Fq@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MzI0Mjktcml0aWth@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
