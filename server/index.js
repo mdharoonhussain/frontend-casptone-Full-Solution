959b584240e2c2c1df59062b4452936443603168
mdharoonhussain <haroonhussain97@gmail.com>
2024-07-25 00:59:32 -0400

    final commit

--- server/index.js
+++ server/index.js
@@ -16,11 +16,11 @@ connectToMongo();
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -197,3 +197,5 @@ const Sign_up = () => {
 }
 
 export default Sign_up;
+
+
