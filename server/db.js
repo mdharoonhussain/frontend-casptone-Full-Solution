b0446dcc01c781b6fefecfb4a5f04f3d6def5ea5
mdharoonhussain <haroonhussain97@gmail.com>
2025-08-26 03:09:50 -0400

    added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:NjI1OC1oYXJvb25t@127.0.0.1:27017";
+// const mongoURI =  "mongodb://root:NjI1OC1oYXJvb25t@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:O3NhD7RqhQqGd8dxgv3OtkZL@172.21.42.9:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
